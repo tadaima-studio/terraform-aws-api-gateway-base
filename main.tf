164e5a5c385daa67382bb67fac6f759df3cc4941
Jay <javier.arranz@tadaima.studio>
2021-01-19 12:07:02 +0100

    dummy method

--- main.tf
+++ main.tf
@@ -61,3 +61,25 @@ resource "aws_api_gateway_method" "this" {
   authorization = "NONE"
   api_key_required = true
 }
+
+
+resource "aws_api_gateway_integration" "this" {
+  rest_api_id = aws_api_gateway_rest_api.this.id
+  resource_id = aws_api_gateway_resource.this.id
+  http_method = aws_api_gateway_method.this.http_method
+  type = "MOCK"
+}
+
+resource "aws_api_gateway_method_response" "200" {
+  rest_api_id = aws_api_gateway_rest_api.this.id
+  resource_id = aws_api_gateway_resource.this.id
+  http_method = aws_api_gateway_method.this.http_method
+  status_code = "200"
+}
+
+resource "aws_api_gateway_integration_response" "this" {
+  rest_api_id = aws_api_gateway_rest_api.this.id
+  resource_id = aws_api_gateway_resource.this.id
+  http_method = aws_api_gateway_method.this.http_method
+  status_code = aws_api_gateway_method_response.200.status_code
+}
