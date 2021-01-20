3d7181b6499c5b7b171cc085cde835f56fa57831
Jay <javier.arranz@tadaima.studio>
2021-01-20 11:38:30 +0100

    new outputs

--- outputs.tf
+++ outputs.tf
@@ -10,3 +10,11 @@ output "execution_arn" {
 output "request_parameters_validator_id" {
   value = aws_api_gateway_request_validator.this.id
 }
+
+output "key_id" {
+  value = aws_api_gateway_api_key.this-key.value
+}
+
+output "invoke_url" {
+  value = aws_api_gateway_deployment.this.invoke_url
+}
